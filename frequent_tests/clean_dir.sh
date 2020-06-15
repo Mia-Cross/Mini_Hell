6d2ce1ae2ee8b86bf7da5baf71a65de65a24b0da
Mia-Cross <leila.marabese@gmail.com>
2020-06-15 03:44:43 +0200

    almost done, testeur upgraded

--- frequent_tests/clean_dir.sh
+++ frequent_tests/clean_dir.sh
@@ -1,10 +1,6 @@
-rm -rf file*
+rm -rf files_with_*
 rm -rf my_*
 rm -rf expected_*
-rm -rf o93
-rm -rf \'\ \\\\\*\o\3\ \3\r\-\3\ \9\4\3\j\ \t\g\"\e\r\p\ \e\w\3\\\ \$\P\O\U\I\C\ \r\k\%\\\|\[\3\2\3\w\\\[\q\3\&\]\]\"\'
-rm -rf \"\\\3\ \\\=\ \f\a\u\x\"
-rm -rf \"\\\3\"
 rm -rf tests
 chmod 777 forbidden
 rm -rf forbidden
